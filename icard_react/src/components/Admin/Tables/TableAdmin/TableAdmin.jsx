d136e5da56f4de5b2255eae59375f82182cb8af0
Justin Arias <jusrtin2010@gmail.com>
2024-01-25 08:50:34 -0500

    avance 2501

---
+++ icard_react/src/components/Admin/Tables/TableAdmin/TableAdmin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Label, Button, Icon, Checkbox } from 'semantic-ui-react';
+import IcTable from "../../../../assets/table.svg"
+import "./TableAdmin.scss"
+
+export function TableAdmin(props) {
+  const { table } = props;
+  return (
+    <div className='table-admin'>
+      <IcTable/>
+    </div>
+  )
+}
