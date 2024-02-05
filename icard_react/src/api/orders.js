af51eb84be213824408b008ddf502c55388ad2c1
Justin Arias <jusrtin2010@gmail.com>
2024-02-04 22:53:52 -0500

    Sistema de pedidos

---
+++ icard_react/src/api/orders.js
@@ -0,0 +1,15 @@
+import { BASE_API } from "../utils/constans";
+export async function getOrdersByTableApi(idTable, status="", ordering=""){
+  try{
+    const tableFilter = `table=${idTable}`
+    const statusFilter = `status=${status}`
+    const closeFilter = 'close=False'
+
+    const url = `${BASE_API}/api/orders/?${tableFilter}&${statusFilter}&${closeFilter}&${ordering}`
+    const response = await fetch(url);
+    const result = await response.json()
+    return result;
+  }catch(error){
+    console.log(error)
+  }
+}
--- icard_react/src/components/Admin/Tables/TablesListAdmin/TablesListAdmin.jsx
+++ icard_react/src/components/Admin/Tables/TablesListAdmin/TablesListAdmin.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button, Icon, Checkbox } from 'semantic-ui-react';
 import { map, size } from 'lodash';
 import { TableAdmin } from "../"
-
-import "./TablesListAdmin"
+import "./TablesListAdmin.scss"
 
 export function TablesListAdmin(props) {
   const { tables } = props;
+  const [ reload, setReload ] = useState(false)
+  const [ autoReload, setAutoReload ] = useState(false)
+  useEffect(()=> {
+    if(autoReload){
+      const autoReloadAction = () => {
+        onReload()
+        setTimeout(()=> {
+          autoReloadAction();
+        }, 5000)
+      }
+      autoReloadAction()
+    }
+  }, [autoReload])
+
+  const onCheckAutoReload = (check) => {
+    if(check){
+      setAutoReload(check)
+    } else {
+      window.location.reload()
+    }
+  }
+
+  const onReload = () => setReload((prev) => !prev)
   return (
-    <div className='tables-list-admin'>
+    <div className='table-list-admin'>
+      <Button primary icon className='table-list-admin__reload' onClick={()=> onReload()}>
+        <Icon name="refresh"></Icon>
+      </Button>
+      <div className='table-list-admin__reload-toggle'>
+        <span>Reload automatico</span>
+        <Checkbox toggle checked={autoReload} onChange={(_,data)=> onCheckAutoReload(data.checked)}/>
+      </div>
       {map(tables, (table)=> (
-        <TableAdmin key={table.number} table={table}/>
+        <TableAdmin key={table.number} table={table} reload={reload}/>
       ))}
     </div>
   )
